2dfcb2d2132274b206da75dce4c7125934360200
quix <quixoticsycophant@gmail.com>
2011-03-16 09:44:12 -0400

    improved readme

--- test/readme_test.rb
+++ test/readme_test.rb
@@ -2,4 +2,4 @@ require_relative 'main'
 require_relative '../devel/levitate'
 Levitate.doc_to_test("README.rdoc",
                      "Synopsis",
-                     "Binding.of_caller shim for Ruby-1.8 code")
+                     "<code>Binding.of_caller</code>")
